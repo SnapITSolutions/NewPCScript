18f1e232aa1d5c50896added661218503c64e97b
Stacey Adams <stacey.adams@snapit.solutions>
2022-11-03 12:16:15 -0500

    Update installer to use packages file

--- README.md
+++ README.md
@@ -10,4 +10,9 @@ Run this command in PowerShell admin shell to install Chocolatey:
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 ```
 
-After Chocolatey is installed, run the `install.ps1` script in the PowerShell admin shell.
+After Chocolatey is installed, close the admin shell and open another one.
+Run this command from the folder containing this repository:
+
+```
+Set-ExecutionPolicy RemoteSigned; .\install.ps1
+```
--- install.ps1
+++ install.ps1
@@ -1,104 +1,6 @@
 # Install new software via Chocolatey (must already be installed)
 
-# Utilities
-
-choco install -y 7zip
-choco install -y chocolateygui
-choco install -y choco-cleaner
-choco install -y less
-
-# Development Tools - Languages, Compilers, etc.
-
-choco install -y nvm # this will manage node/npm installs
-choco install -y jq
-choco install -y python3
-# choco install -y pip # can't be installed before the reboot
-choco install -y yarn
-
-# Development Tools - Editors, IDEs
-
-choco install -y markdownpad2
-choco install -y notepadplusplus
-choco install -y vscode
-choco install -y vscode-powershell
-choco install -y vscode-icons
-choco install -y vscode-eslint
-
-# Development Tools - Source Control
-
-choco install -y git
-choco install -y git-fork
-choco install -y git-lfs
-choco install -y meld
-choco install -y TortoiseGit
-
-# Development Tools - Cloud Deployment
-
-choco install -y docker-cli
-choco install -y docker-compose
-choco install -y docker-desktop
-choco install -y postman
-
-# Development Tools - Database Management
-
-choco install -y dbeaver
-choco install -y mongodb
-choco install -y mongodb-compass
-choco install -y pgadmin4
-choco install -y postgresql
-choco install -y mysql
-choco install -y mysql.workbench
-choco install -y SQLite
-choco install -y sqlite-studio.portable
-choco install -y sqlitebrowser
-
-# Command Line Terminals
-
-choco install -y Cmder
-
-# Network/Internet
-
-choco install -y curl
-choco install -y Firefox
-choco install -y Wget
-choco install -y filezilla
-choco install -y putty
-choco install -y winscp.install
-
-# Fonts
-
-choco install -y ampfontviewer
-
-choco install -y anonymouspro
-choco install -y archivo-narrow.font
-choco install -y bitstreamverafonts
-choco install -y cascadiacode
-choco install -y cascadiacodeitalic
-choco install -y cascadiamono
-choco install -y cascadiamonoitalic
-choco install -y croscorefonts-font
-choco install -y crosextrafonts-caladea-font
-choco install -y crosextrafonts-carlito-font
-choco install -y dejavufonts
-choco install -y fantasque-sans.font
-choco install -y fira
-choco install -y FiraCode
-choco install -y go-fonts
-choco install -y hackfont
-choco install -y inconsolata
-choco install -y inter
-choco install -y jetbrainsmono
-choco install -y lato
-choco install -y liberationfonts
-choco install -y libre-baskerville.font
-choco install -y montserrat.font
-choco install -y opensans
-choco install -y oswald.font
-choco install -y oxygenmono
-choco install -y public-sans.font
-choco install -y roboto.font
-choco install -y SourceCodePro
-choco install -y ubuntu.font
+choco install -y --stop-on-first-failure packages.config
 
 # Install Node.JS
 
@@ -112,10 +14,6 @@ npm install -g @angular/cli@6
 # Install Typescript
 
 npm install -g typescript
-
-# Install Powershell Git prompt
-
-Install-Module posh-git
 
 # Install Ubuntu on WSL
 
